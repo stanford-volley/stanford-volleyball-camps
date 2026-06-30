f644b0f1b3899545451a3f495b5dae9ad4b9f41e
stanford-volley <volleyball@stanford.edu>
2026-06-30 10:32:19 -0700

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,1 +1,6 @@
+import { createClient } from "@supabase/supabase-js";
 
+const supabaseUrl = "https://zjdnzipqdacvlcvfjdxb.supabase.co";
+const supabaseKey = "sb_publishable_SwCgMI_lBgks7TCBWJBJvg_DZdmE7Qo";
+
+export const supabase = createClient(supabaseUrl, supabaseKey);
